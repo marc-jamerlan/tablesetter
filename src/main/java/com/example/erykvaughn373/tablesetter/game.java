6f8f2e7dc7c6b7167b226cfa17432fb4d6e06aca
ErykVaughn <48067943+ErykVaughn@users.noreply.github.com>
2019-03-06 14:43:04 -0800

    Add files via upload
    this is the table setter source so far

---
+++ src/main/java/com/example/erykvaughn373/tablesetter/game.java
@@ -0,0 +1,19 @@
+package com.example.erykvaughn373.tablesetter;
+
+import java.util.List;
+
+public class game {
+    private String name;
+
+    public game(){
+        this.name = "Default Name";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
